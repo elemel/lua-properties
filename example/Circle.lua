81f96d6fa4bfc0601dbe35dc9dba5536ff2ebf87
Mikael Lind <elemel@elemel.se>
2020-07-20 21:55:54 +0200

    Improve example

--- example/Circle.lua
+++ example/Circle.lua
@@ -13,6 +13,28 @@ function M.new(center, radius)
   return setmetatable({_center = center, _radius = radius}, M)
 end
 
+function M._getters:center()
+  return self._center
+end
+
+function M._setters:center(center)
+  assert(type(center) == "table" and getmetatable(center) == Point)
+  self._center = center
+end
+
+function M._getters:radius()
+  return self._radius
+end
+
+function M._setters:radius(radius)
+  assert(type(radius) == "number" and radius >= 0)
+  self._radius = radius
+end
+
+function M._getters:circumference()
+  return 2 * math.pi * self._radius
+end
+
 function M._getters:area()
   return math.pi * self._radius * self._radius
 end
--- example/main.lua
+++ example/main.lua
@@ -2,7 +2,10 @@ local Circle = require("example.Circle")
 local Point = require("example.Point")
 
 local point = Point.new(1, 2)
-assert(tostring(point) == "(1, 2)")
+point.x, point.y = point.y, point.x
+assert(tostring(point) == "(2, 1)")
 
-local circle = Circle.new(point, 3)
+local circle = Circle.new(point)
+circle.radius = 3
+assert(math.abs(circle.circumference - 18.849555921539) < 1e-12)
 assert(math.abs(circle.area - 28.274333882308) < 1e-12)
